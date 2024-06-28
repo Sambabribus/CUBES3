3f9a7452d493b25de36793f441ec1c07e42fd51e
Sflegioner <117868151+Sflegioner@users.noreply.github.com>
2024-06-28 16:57:10 +0200

    MergeConflict

--- MobileApp/CookEco/Models/Models.cs
+++ MobileApp/CookEco/Models/Models.cs
@@ -4,23 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite;
+using System.Text.Json.Serialization;
 
 namespace CookEco.Models
 {
-        public class User
-        {
-            [PrimaryKey, AutoIncrement]
-            public int Id { get; set; }
-            public string Username { get; set; }
-            public string Password { get; set; }
-        }
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [JsonPropertyName("username_user")]
+        public string Username { get; set; }
+        [JsonPropertyName("pwd_user")]
+        public string Password { get; set; }
+    }
 
-        public class Recipe
-        {
-            [PrimaryKey, AutoIncrement]
-            public int Id { get; set; }
-            public string Title { get; set; }
-            public string Description { get; set; }
-            public string ImagePath { get; set; }
-        }
+    public class UsersResponse
+    {
+        [JsonPropertyName("records")]
+        public List<User> Records { get; set; }
+    }
+
+    public class Recipe
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+    }
 }
